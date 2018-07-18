3418c8bc454bc02bc13a68bb7baa524caef71ce2
DESKTOP-LE46UB1\HAN <kerorozzzz@naver.com>
2018-07-18 09:40:05 +0900

    0718 한영진

---
+++ final_project/src/main/java/kr/pe/mododa/write/controller/WriteController.java
@@ -0,0 +1,15 @@
+package kr.pe.mododa.write.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WriteController {
+
+		@Autowired
+		@RequestMapping(value="/write.do")
+		public String writeReady() {
+			return "write/writePage";
+		}
+}
